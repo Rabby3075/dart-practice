bb962dee47f4dfafcb07d37e762a923dbe12e3ed
Rabby3075 <rashed.rabby43@gmail.com>
2023-08-05 23:03:15 +0600

    assignment 1

---
+++ assignment1.dart
@@ -0,0 +1,10 @@
+void main() {
+  const int a = 10;
+  const int b = 5;
+  print('a = $a , b = $b');
+  print('Addition: $a+$b=${a + b}');
+  print('Subtraction: $a-$b=${a - b}');
+  print('Multiplication: $a x $b=${a * b}');
+  print('Division: $a/$b=${a / b}');
+  print('Modulus: $a%$b=${a % b}');
+}
